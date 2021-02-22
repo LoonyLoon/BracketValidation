904a586a1eacfa7f3429582be5efa79c4d53c6b4
Evgen_Tolchanov <tolchanove@mail.ru>
2021-02-22 22:27:14 +0300

    Init project

---
+++ BracketValidation/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+//using namespace std;
+
+
+class BracketValidation
+{
+
+public:
+	std::string m_TempString;
+
+public:
+	BracketValidation()
+	{
+		we
+	}
+	~BracketValidation()
+	{
+
+	};
+
+	bool validation(std::string m_TempString)
+	{
+
+	};
+
+
+};
+
+
+int main()
+{
+   
+}
