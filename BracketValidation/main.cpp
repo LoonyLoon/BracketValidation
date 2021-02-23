e4fd8412f643c3c93891488b5cfc34aee0e4244c
Evgen_Tolchanov <tolchanove@mail.ru>
2021-02-23 21:22:43 +0300

    Release

--- BracketValidation/main.cpp
+++ BracketValidation/main.cpp
@@ -1,27 +1,67 @@
 #include <iostream>
 #include <string>
-//using namespace std;
+#include <map>
+#include <stack>	
 
 
 class BracketValidation
 {
 
-public:
+private:	
+	std::map<char, char> brackets = {
+		{')', '('},
+		{'}', '{'},
+		{']', '['}
+	};
+	std::stack<char> st;
 	std::string m_TempString;
 
+
 public:
-	BracketValidation()
+	BracketValidation(std::string String)
 	{
-		we
+		for (int i = 0; i <= String.size(); i++) // Removing unnecessary charackters besides brackets
+		{
+			if (String[i] == '(' || String[i] == ')' ||
+				String[i] == '[' || String[i] == ']' ||
+				String[i] == '{' || String[i] == '}')
+			{;
+			
+			m_TempString += String.substr(i, 1);
+			}
+		}
 	}
 	~BracketValidation()
 	{
 
 	};
 
-	bool validation(std::string m_TempString)
+	bool validation()
 	{
+		
+		
+		if (m_TempString.size() % 2 != 0) // Parity check 
+		{
+			return false;
+		}
 
+		for (auto ch : m_TempString) 
+		{
+			if (ch == '(' || ch == '{' || ch == '[') // if brackets are open then put on the stack
+			{
+				st.push(ch); 
+			}
+			else if (ch == ')' || ch == '}' || ch == ']') 
+			{
+				if (!st.empty() && st.top() == brackets[ch])
+				{
+					st.pop(); //if brackets are true and stack is't empty then removing pair brackets from the stack 
+				}
+				else
+					return false;
+			}
+		}
+		return st.empty();
 	};
 
 
@@ -30,5 +70,20 @@ public:
 
 int main()
 {
-   
+	std::string str;
+	std::cout << "Enter expression" << std::endl;
+	std::cin >> str;
+
+	BracketValidation* obj = new BracketValidation(str);
+	if (obj->validation())
+	{
+		std::cout << "True";
+	}
+	else {
+
+		std::cout << "False";
+	}
+
+	delete obj;
+	return 0;
 }
